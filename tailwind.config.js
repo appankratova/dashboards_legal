83912bbad80392d6456b051933b09a2e1e4fcaa5
appankratova <118073125+appankratova@users.noreply.github.com>
2025-11-26 13:10:47 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        slate: {
+          850: '#151f32', // Custom dark shade
+        }
+      }
+    },
+  },
+  plugins: [],
+}
