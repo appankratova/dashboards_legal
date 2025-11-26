1885d82e1bdf105d799a29b505bb554cd0dc030e
appankratova <118073125+appankratova@users.noreply.github.com>
2025-11-26 12:42:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This ensures assets are linked relatively, making it work on any GitHub Pages path (e.g. username.github.io/repo-name)
-  base: './', 
+  // Важно: имя репозитория со слэшами с обеих сторон
+  base: '/dashboards_legal/', 
 });
